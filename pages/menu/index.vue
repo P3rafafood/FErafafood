62292fe409edb9c9cee8c949209fcdc4a6e77bd6
Ratusukmakomala <ratukomala28@gmail.com>
2023-11-29 16:01:34 +0700

    code tambah pesanan

--- pages/menu/index.vue
+++ pages/menu/index.vue
@@ -75,7 +75,7 @@ function addToCart(menu) {
                 <h5 class="card-title">{{ food.name }}<span class="float-end"><Rupiah :angka="food.price"/></span></h5>
                 <p class="card-text">{{ food.desc }}</p>
                 <p class="card-text"><small class="text-body-secondary">&nbsp;</small></p>
-                <button type="button" class="btn btn-warning float-end text-white mb-3" @click="addToCart(food)">Tambah Pesanan</button>
+                <button type="button" class="btn btn-warning float-end text-white mb-3" @click="addToCart(food)">Tambah Pesanan Anda</button>
               </div>
             </div>
           </div>
@@ -116,4 +116,6 @@ function addToCart(menu) {
   </div>
 </template>
 
+
 <style scoped></style>
+
