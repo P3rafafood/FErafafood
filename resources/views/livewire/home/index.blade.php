b6c3778a4fbe6c7f7e150ad50eebf033b8c6fa39
Ratusukmakomala <ratukomala28@gmail.com>
2023-12-15 14:02:39 +0700

    lagi

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-5 col-lg-6 text-center">
         <h1 class="text-primary">P3 RAFA <br class="d-none d-xl-block" />ORDER FOOD<br /></h1>
-        <p class="mb-5 fs-5">Setiap Gigitannya Memiliki Kelezatan Yang Berbeda, <br class="d-none d-xl-block" />harga
+        <p class="mb-5 fs-5">Setiap Gigitannya Memiliki Kelezatan Yang Berbeda, <br class="d-none d-xl-block" />Harga
             Terjangkau!</p>
     </div>
     <div class="col-md-7 col-lg-6">
