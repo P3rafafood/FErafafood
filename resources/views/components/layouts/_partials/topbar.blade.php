ebaba0a340f24f57adf93670aa42cf0c58a7d41e
isnaenti <isnaenti16@gmail.com>
2023-12-19 10:37:41 +0700

    Edit topbar Logout

--- resources/views/components/layouts/_partials/topbar.blade.php
+++ resources/views/components/layouts/_partials/topbar.blade.php
@@ -57,7 +57,7 @@
                         <li>
                             <a class="dropdown-item" href="{{ route('admin.logout') }}">
                                 <i class="icon-mid bi bi-box-arrow-left me-2"></i>
-                                Logout
+                                LogOut
                             </a>
                         </li>
                     </ul>
