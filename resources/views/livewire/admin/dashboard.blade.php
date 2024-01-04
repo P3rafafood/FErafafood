dc2b0aa848214f39d85f733a9e38fc7e80406445
isnaenti <isnaenti16@gmail.com>
2024-01-04 10:19:13 +0700

    edit pesanan notif keranjang

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -45,7 +45,7 @@
                                 </div>
                             </div>
                             <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                <h6 class="text-muted font-semibold">Semua pesanan</h6>
+                                <h6 class="text-muted font-semibold">All Orders</h6>
                                 <h6 class="font-extrabold mb-0">{{ $all_orders }}</h6>
                             </div>
                         </div>
