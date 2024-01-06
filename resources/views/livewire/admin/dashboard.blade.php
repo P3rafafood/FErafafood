48c40b12048f1b9a4ee77f4607b90ccde5e3ddf6
Ratusukmakomala <ratukomala28@gmail.com>
2024-01-06 12:32:51 +0700

    mengedit navbar menu

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -11,7 +11,7 @@
                                 </div>
                             </div>
                             <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                <h6 class="text-muted font-semibold">Menu Makanan</h6>
+                                <h6 class="text-muted font-semibold">Menu</h6>
                                 <h6 class="font-extrabold mb-0">{{ $menu }}</h6>
                             </div>
                         </div>
